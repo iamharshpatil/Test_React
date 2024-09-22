1ec11414e29c7a214a7521ffbe6ebbc3b849ffcb
Harshwardhan Patil <harshwardhanpatil340@gmail.com>
2024-09-22 23:19:03 +0530

    How to show JSON with the help of map()

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
-import Home from "./Home"
-import Nav from "./Nav"
+import About from "./About";
+import Home from "./Home";
+import Nav from "./Nav";
 
-const App = ()=>{
-  return (
-  <div>
-   <Home/>
-   <Nav/>
-  </div>  ) 
-}
+const App = () => {
+  let user = [
+    {
+      name: "Harshwardhan",
+      age: 27,
+      city: "Bangalore",
+    },
+    { name: " Wang", 
+      age: 25,
+       city: "bhopal" },
+  ];
 
-export default App
+  let r =  user.map((u,i)=> <h1 key={i}>{u.name}</h1> );
+  return <div>
+        {r}
+  </div>;
+};
+
+export default App;
